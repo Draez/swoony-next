bbc2d40e44a8d495599a00ed8c8e0d1b94852bb4
Oskar Lehtonen <oskar.lehtonen@oldigital.fi>
2025-05-26 15:57:45 +0300

    Add SV to kiitos rekisteröitymisstä

--- pages/thank-you-for-registering.tsx
+++ pages/thank-you-for-registering.tsx
@@ -15,9 +15,12 @@ export default function ThankYou() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <div className="max-w-2xl mx-auto text-center mt-12">
-                <h1 className="text-4xl font-bold text-main-blue mb-6">Kiitos rekisteröitymisestä!</h1>
+                <h1 className="text-4xl font-bold text-main-blue mb-6">
+                    Kiitos rekisteröitymisestä! / Tack för registreringen!
+                </h1>
                 <p className="text-lg text-light-blue mb-8">
-                    Sähköpostisi on nyt aktivoitu ja voit kirjautua sovellukseen.
+                    Sähköpostisi on nyt aktivoitu ja voit kirjautua sovellukseen. /
+                    Din e-post är nu aktiverad och du kan logga in i appen.
                 </p>
             </div>
         </main>
