4c631d3d205bc97dd92e2fdc9a071e69eee446fb
Oskar Lehtonen <oskar.lehtonen@oldigital.fi>
2025-11-03 13:49:01 +0200

    FB

--- lib/fb.ts
+++ lib/fb.ts
@@ -1,13 +1,15 @@
-export const facebookPixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID!;
+export const facebookPixelId = "138553019289982";
 
 export const trackFbPageView = async () => {
   const { default: ReactPixel } = await import("react-facebook-pixel");
+  console.log(facebookPixelId);
   ReactPixel.init(facebookPixelId);
   ReactPixel.pageView();
 };
 
 export const trackLead = async (data: { email: string; name: string }) => {
   const { default: ReactPixel } = await import("react-facebook-pixel");
+  console.log("Tracklead", facebookPixelId);
   ReactPixel.init(facebookPixelId);
   ReactPixel.track("Lead", data);
 };
